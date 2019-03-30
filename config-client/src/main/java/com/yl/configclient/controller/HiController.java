9be7f51641f982ba1f73ddecb0147d76ffa0a49c
yuanzhichao <yuanzhichao@qilianxing.com.cn>
2019-03-30 13:45:11 +0800

    config init

---
+++ config-client/src/main/java/com/yl/configclient/controller/HiController.java
@@ -0,0 +1,18 @@
+package com.yl.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping("hi")
+    public String hi(){
+        return foo;
+    }
+
+}
